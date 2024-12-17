e291dbd36030ce2497e9c4d6e313ed03ed4ba9c4
Ligh0x74 <1185582263@qq.com>
2024-12-17 18:47:14 +0800

    init

---
+++ src/main/java/com/example/lcpredictor/LcPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lcpredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcPredictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcPredictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lcpredictor/LcPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lcpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcPredictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
